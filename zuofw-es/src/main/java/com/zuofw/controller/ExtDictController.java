d908d58c4e3ddbe8a85118826b089c104dfe2947
zuowei <2354796263@qq.com>
2024-12-16 14:56:12 +0800

    feat：动态添加分词、停用词

---
+++ zuofw-es/src/main/java/com/zuofw/controller/ExtDictController.java
@@ -0,0 +1,24 @@
+package com.zuofw.controller;
+
+import com.zuofw.service.ExtDictService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+@Slf4j
+public class ExtDictController {
+
+    @Resource
+    private ExtDictService extDictService;
+
+    @GetMapping("/ext/dict")
+    public ResponseEntity<String> extDict() {
+        return extDictService.getHotUpdateExtDict();
+    }
+
+}
